e8e6c616d73e9372f9c01c4ed21bd935a77cc295
James <tillurdizzy@live.com>
2016-03-15 11:05:37 -0500

    mar15 office

--- public/client/views/review.ctrl.js
+++ public/client/views/review.ctrl.js
@@ -5,9 +5,32 @@ app.controller('ReviewCtrl',['$scope','$state','ClientSharedSrvc',function ($sco
 	var ME = this;
 	
 	ME.C = ClientSharedSrvc;
+	ME.UpgradeRidgeCaps = {};
+	ME.UpgradeDripEdge = {};
+	ME.baseLineTotal = "5855.76";
+	ME.grandTotal = ME.baseLineTotal;
 
-	
+	ME.UpgradeFieldShingleSelection = "GAFTBRNAT";
+	ME.UpgradeRidgeSelection = "Default";
+	ME.UpgradeValleySelection = "Default";
+	ME.UpgradeTrimSelection = "Default";
 
+	ME.showUpgrades = {field:true,ridge:false,valley:false,trim:false};
+	ME.shingleUpgradePrices = {GAFTBRNAT:"0",GAFTBRHD:"248",GAFTBRUHD:"302",GAFTBRCOOL:"389",GAFARMSH:"633",GAFGRNSEQ:"677"};
+	ME.ridgeUpgradePrices = {Default:"0",Upgrade:"248"};
+	ME.valleyUpgradePrices = {Default:"0",Upgrade:"102"};
+	ME.trimUpgradePrices = {Default:"0",Upgrade:"133"};
+
+
+	ME.calculateTotal = function(){
+		var fieldUpgradeCost = Number(ME.shingleUpgradePrices[ME.UpgradeFieldShingleSelection]);
+		var ridgeUpgradeCost = Number(ME.ridgeUpgradePrices[ME.UpgradeRidgeSelection]);
+		var valleyUpgradeCost = Number(ME.valleyUpgradePrices[ME.UpgradeValleySelection]);
+		var trimUpgradeCost = Number(ME.trimUpgradePrices[ME.UpgradeTrimSelection]);
+		var base = Number(ME.baseLineTotal);
+		ME.grandTotal = base + fieldUpgradeCost + ridgeUpgradeCost + valleyUpgradeCost + trimUpgradeCost;
+
+	}
 	
 	
 	$scope.$watch('$viewContentLoaded', function() {
@@ -16,5 +39,7 @@ app.controller('ReviewCtrl',['$scope','$state','ClientSharedSrvc',function ($sco
        		$state.transitionTo('login');
        }
     });
+
+   
 
  }]);
--- public/views/clients/client-new-view.ctrl.js
+++ public/views/clients/client-new-view.ctrl.js
@@ -1,24 +1,24 @@
 'use strict';
-app.controller('NewClientViewCtrl',['$state',function ($state) {
+app.controller('NewClientViewCtrl', ['$state', function($state) {
 
-	var ME = this;
+    var ME = this;
     ME.ClientType = "Individual";
-   
-    ME.goJobs = function(){
-      $state.transitionTo("jobs");
+
+    ME.goJobs = function() {
+        $state.transitionTo("jobs");
     };
 
-    ME.goProperties = function(){
-      $state.transitionTo("properties");
+    ME.goProperties = function() {
+        $state.transitionTo("properties");
     };
-   
-    ME.submitClientType=function(){
-        if( ME.ClientType=="Individual"){
-           $state.transitionTo("addNewClient.individual");
-        }else{
-             $state.transitionTo("addNewClient.organization");
+
+    ME.submitClientType = function() {
+        if (ME.ClientType == "Individual") {
+            $state.transitionTo("addNewClient.individual");
+        } else {
+            $state.transitionTo("addNewClient.organization");
         };
     };
 
 
- }]);
+}]);
